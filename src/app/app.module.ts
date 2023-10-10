3c32f83c403672bb36f36cc7aade8ed63f292514
Pierre Benest <pierre.benest@beta.gouv.fr>
2023-10-10 12:01:41 +0200

    merge2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { AppRoutingModule } from './app-routing.module';
 import { TodoModule } from './todo/todo.module';
-=======
-import { TodoModule } from 'src/todo/todo.module';
->>>>>>> 6c8774c22cb7effc638bb88bd1c1fd441862848b
 
 @NgModule({
   declarations: [
@@ -15,12 +11,8 @@ import { TodoModule } from 'src/todo/todo.module';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     AppRoutingModule,
     TodoModule
-=======
-    TodoModule,
->>>>>>> 6c8774c22cb7effc638bb88bd1c1fd441862848b
   ],
   providers: [],
   bootstrap: [AppComponent]
